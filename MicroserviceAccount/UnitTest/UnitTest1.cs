39ab0db2df490c6b286d10aa7c2cea79b129cc62
ngynkhnhlnh <nguyenkhanhlinh2752001@gmail.com>
2022-12-13 23:22:25 +0700

    fix 2

---
+++ MicroserviceAccount/UnitTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public string CreateMessage1()
+        {
+            return "linh";
+        }
+
+        public string CreateMessage2()
+        {
+            return "yen";
+        }
+
+        public string CreateMessage3()
+        {
+            return "ngoc";
+        }
+
+        public string CreateMessage4()
+        {
+            return "nhi";
+        }
+
+        public string CreateMessage5()
+        {
+            return "huong";
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("linh", CreateMessage1());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("yen", CreateMessage2());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("ngoc", CreateMessage3());
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual("nhi", CreateMessage4());
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual("huong", CreateMessage5());
+        }
+    }
+}
