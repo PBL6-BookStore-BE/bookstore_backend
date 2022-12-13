66f268879ec6d57b962a3ba7a3d84abd1b766bd3
ngynkhnhlnh <73626361+nguyenkhanhlinh2752001@users.noreply.github.com>
2022-12-13 22:48:59 +0700

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,9 +9,25 @@ namespace ConsoleApp1
             Console.WriteLine(CreateMessage());
         }
 
-        public static String CreateMessage()
+        public static String CreateMessage1()
         {
-            return "nhim";
+            return "linh";
+        }
+        public static String CreateMessage2()
+        {
+            return "yen";
+        }
+        public static String CreateMessage3()
+        {
+            return "ngoc";
+        }
+        public static String CreateMessage4()
+        {
+            return "nhi";
+        }
+        public static String CreateMessage5()
+        {
+            return "huong";
         }
     }
 }
