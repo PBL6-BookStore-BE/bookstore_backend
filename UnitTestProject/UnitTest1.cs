2ac34db40eb3a3f28fcff78c605532eb72652afd
ngynkhnhlnh <73626361+nguyenkhanhlinh2752001@users.noreply.github.com>
2022-12-13 22:47:30 +0700

    Update UnitTest1.cs

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -9,7 +9,30 @@ namespace UnitTestProject
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("nhim", Program.CreateMessage());
+            Assert.AreEqual("linh", Program.CreateMessage1());
+        }
+        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("yen", Program.CreateMessage2());
+        }
+        
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("ngoc", Program.CreateMessage3());
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual("nhi", Program.CreateMessage4());
+        }
+        
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual("huong", Program.CreateMessage5());
         }
     }
 }
